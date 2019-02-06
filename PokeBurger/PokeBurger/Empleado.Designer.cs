380bce53a798427a72e64476efc03548760c696f
chrixu97 <39270560+chrixu97@users.noreply.github.com>
2019-02-06 20:42:25 +0100

    Add files via upload

---
+++ PokeBurger/PokeBurger/Empleado.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace PokeBurger
+{
+    partial class Empleado
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.bAgregarEmpleado = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.bModificar = new System.Windows.Forms.Button();
+            this.bNominas = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(255)))), ((int)(((byte)(132)))));
+            this.label1.Location = new System.Drawing.Point(0, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1357, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(255)))), ((int)(((byte)(132)))));
+            this.label2.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(1115, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(221, 36);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "EMPLEADOS";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(255)))), ((int)(((byte)(132)))));
+            this.label3.Location = new System.Drawing.Point(27, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(1356, 729);
+            this.label3.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(43, 107);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1047, 699);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(255)))), ((int)(((byte)(132)))));
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Location = new System.Drawing.Point(1112, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(278, 342);
+            this.label4.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(255)))), ((int)(((byte)(132)))));
+            this.label5.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(1108, 107);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "FILTROS";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Verdana", 10F);
+            this.textBox1.Location = new System.Drawing.Point(43, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(749, 28);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.BackgroundImage = global::PokeBurger.Properties.Resources.lupa_de_busqueda;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatAppearance.BorderSize = 3;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(792, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(29, 30);
+            this.button1.TabIndex = 11;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // bAgregarEmpleado
+            // 
+            this.bAgregarEmpleado.BackgroundImage = global::PokeBurger.Properties.Resources.AgregarEmpleado;
+            this.bAgregarEmpleado.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bAgregarEmpleado.FlatAppearance.BorderSize = 0;
+            this.bAgregarEmpleado.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bAgregarEmpleado.Location = new System.Drawing.Point(1112, 572);
+            this.bAgregarEmpleado.Name = "bAgregarEmpleado";
+            this.bAgregarEmpleado.Size = new System.Drawing.Size(93, 74);
+            this.bAgregarEmpleado.TabIndex = 9;
+            this.bAgregarEmpleado.UseVisualStyleBackColor = true;
+            this.bAgregarEmpleado.Click += new System.EventHandler(this.bAgregarEmpleado_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::PokeBurger.Properties.Resources.Eliminar;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(1112, 732);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 74);
+            this.button2.TabIndex = 8;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // bModificar
+            // 
+            this.bModificar.BackgroundImage = global::PokeBurger.Properties.Resources.Modificar;
+            this.bModificar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bModificar.FlatAppearance.BorderSize = 0;
+            this.bModificar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bModificar.Location = new System.Drawing.Point(1112, 652);
+            this.bModificar.Name = "bModificar";
+            this.bModificar.Size = new System.Drawing.Size(93, 74);
+            this.bModificar.TabIndex = 7;
+            this.bModificar.UseVisualStyleBackColor = true;
+            this.bModificar.Click += new System.EventHandler(this.bModificar_Click);
+            // 
+            // bNominas
+            // 
+            this.bNominas.BackgroundImage = global::PokeBurger.Properties.Resources.Nominas;
+            this.bNominas.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bNominas.FlatAppearance.BorderSize = 0;
+            this.bNominas.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.bNominas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bNominas.Location = new System.Drawing.Point(1112, 492);
+            this.bNominas.Name = "bNominas";
+            this.bNominas.Size = new System.Drawing.Size(93, 74);
+            this.bNominas.TabIndex = 6;
+            this.bNominas.UseVisualStyleBackColor = true;
+            this.bNominas.Click += new System.EventHandler(this.bNominas_Click);
+            // 
+            // Empleado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(1383, 845);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.bAgregarEmpleado);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.bModificar);
+            this.Controls.Add(this.bNominas);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Empleado";
+            this.Text = "Empleado";
+            this.Load += new System.EventHandler(this.Empleado_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button bNominas;
+        private System.Windows.Forms.Button bModificar;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button bAgregarEmpleado;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
